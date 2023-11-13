430f0864ceac9600b640b92ace3d73955c92d21f
annejing01 <annejing01@gmail.com>
2023-11-12 21:50:25 -0500

    fixed endpoints for create and update

--- petpal/accounts/urls.py
+++ petpal/accounts/urls.py
@@ -4,29 +4,22 @@ from . import notifications
 
 from rest_framework.routers import DefaultRouter
 
-# from .views import ShelterViewSet
-
-
 app_name = "accounts"
 
-# router = DefaultRouter()
-# router.register(r'profile', ShelterViewSet, basename='user-profile')
 
 urlpatterns = [
-    # path('seeker/', views.RegistrationView.as_view(), name='seeker_register'),
+    path('<str:user_type>/', views.RegistrationUpdateView.as_view(), name='register'),
+    path('shelter/details/<int:pk>/', views.ShelterProfileView.as_view(), name='shelter-details'),
+    path('shelter/all/', views.AllShelters.as_view(), name='shelter-all'),
 
+    # path('seeker/', views.RegistrationView.as_view(), name='seeker_register'),
     # path('seeker', views.RegistrationView.as_view(), name = "seeker_create"), 
     # path('seeker/edit', views.UserUpdateView.as_view(), name='seeker_update'), 
-    path('<str:user_type>/edit/', views.UpdateView.as_view(), name='update'),
+    # path('<str:user_type>/edit/', views.UpdateView.as_view(), name='update'),
     # path('seeker/details', views.ProfileView.as_view(), name='profile'),
-
     # path('shelter', views.RegistrationView.as_view(), name = "seeker_create"), 
     # path('shelter/details', views.ShelterProfileView.as_view(), name='shelter-details'),
-    path('shelter/details/<int:pk>/', views.ShelterProfileView.as_view(), name='shelter-details'),
     # path('shelter/edit', views.ShelterUpdateView.as_view(), name='shelter-update'), 
-    path('<str:user_type>/', views.RegistrationView.as_view(), name='register'),
-
-    path('shelter/all/', views.AllShelters.as_view(), name='shelter-all'),
     # path('', include(router.urls)),
     
     path('notifications/<int:pk>/', notifications.NotificationUpdate.as_view(), name = "notif_update"),
--- petpal/accounts/views.py
+++ petpal/accounts/views.py
@@ -10,9 +10,10 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.exceptions import PermissionDenied
 
-class RegistrationView(CreateAPIView):
-    permission_classes = [AllowAny]
-    authentication_classes = []
+
+class RegistrationUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get_serializer_class(self):
         user_type = self.kwargs.get('user_type', 'default')
         if user_type == 'shelter':
@@ -20,72 +21,8 @@ class RegistrationView(CreateAPIView):
         else:
             return userSerializer
 
-
-    def create(self, request, *args, **kwargs):
-        user_type = self.kwargs.get('user_type', 'default')
-
-        print(user_type)
-
-        if user_type == 'seeker':
-            serializer_class = userSerializer
-            serializer = serializer_class(data=request.data)
-
-        else:
-            serializer_class = shelterSerializer
-            serializer = serializer_class(data=request.data)
-        
-        if serializer.is_valid():
-            account = serializer.save()
-            user_id = account.id
-            if user_type == 'seeker': 
-                data = {
-                    "response": "Success",
-                    "user_id": user_id,
-                    "username": account.username,
-                    "email": account.email,
-                    "first_name": account.first_name,
-                    "last_name": account.last_name,
-                    "phone": account.phone,
-                    "is_shelter": account.is_shelter
-                }
-            else: 
-                print(serializer)
-                data = {
-                    "response": "Success",
-                    "user_id": account.user.id,  
-                    "username": account.user.username, 
-                    "shelter_name": account.user.first_name, 
-                    "charity_id": account.charity_id,
-                    "address": account.address,
-                    "email": account.user.email,  
-                    "phone": account.user.phone,
-                    "is_shelter": account.user.is_shelter 
-                }
-
-            return Response(data)
-        else:
-            return Response(serializer.errors)
-        
-
-from rest_framework import status
-from django.contrib.auth import update_session_auth_hash
-
-class UpdateView(generics.RetrieveUpdateAPIView):
-
-    def get_serializer_class(self):
-        user_type = self.kwargs.get('user_type', 'default')
-        if user_type == 'seeker':
-            return userSerializer
-        else:
-            return shelterSerializer
-
-    # serializer_class = get_serializer_class
-    permission_classes = [IsAuthenticated]
-
-
     def get_object(self):
         user_type = self.kwargs.get('user_type', 'default')
-
         is_shelter = self.request.user.is_shelter
 
         if user_type == 'seeker' and not is_shelter:
@@ -94,10 +31,72 @@ class UpdateView(generics.RetrieveUpdateAPIView):
             return self.request.user.shelter_profile
         else:
             raise PermissionDenied("You are not authorized to edit this profile")
-    
-    def update(self, request, *args, **kwargs):
-        kwargs['partial'] = True 
-        return super().update(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        # Registration logic
+        user_type = self.kwargs.get('user_type', 'default')
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            account = serializer.save()
+            data = self.create_response_data(account, user_type)
+            return Response(data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def create_response_data(self, account, user_type):
+        # Create the response data based on user_type
+        if user_type == 'seeker':
+            user_id = account.id
+            return {
+                "response": "Success",
+                "user_id": user_id,
+                "username": account.username,
+                "email": account.email,
+                "first_name": account.first_name,
+                "last_name": account.last_name,
+                "phone": account.phone,
+                "is_shelter": account.is_shelter
+                
+            }
+        else:
+            user_id = account.user.id
+            return {
+                "response": "Success",
+                "user_id": account.user.id,  
+                "username": account.user.username, 
+                "shelter_name": account.user.first_name, 
+                "charity_id": account.charity_id,
+                "address": account.address,
+                "email": account.user.email,  
+                "phone": account.user.phone,
+                "is_shelter": account.user.is_shelter 
+            }
+
+    def put(self, request, *args, **kwargs):
+        # Update logic
+        user_type = self.kwargs.get('user_type', 'default')
+        self.permission_classes = [IsAuthenticated]
+        self.check_permissions(request)
+
+        instance = self.get_object()
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(instance, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            account = serializer.save()
+            data = self.create_response_data(account, user_type)
+            return Response(data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [AllowAny()]
+        return [IsAuthenticated()]
     
 
 
@@ -112,8 +111,6 @@ class ProfileView(APIView):
         return Response(serializer.data)
 
 
-
-
 class ShelterProfileView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
     queryset = ShelterProfile.objects.all()
@@ -125,3 +122,103 @@ class AllShelters(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     queryset = ShelterProfile.objects.all()
     serializer_class = shelterSerializer
+
+
+
+
+
+
+
+
+
+
+
+# class RegistrationView(CreateAPIView):
+#     permission_classes = [AllowAny]
+#     authentication_classes = []
+#     def get_serializer_class(self):
+#         user_type = self.kwargs.get('user_type', 'default')
+#         if user_type == 'shelter':
+#             return shelterSerializer
+#         else:
+#             return userSerializer
+
+
+#     def create(self, request, *args, **kwargs):
+#         user_type = self.kwargs.get('user_type', 'default')
+
+
+#         if user_type == 'seeker':
+#             serializer_class = userSerializer
+#             serializer = serializer_class(data=request.data)
+
+#         else:
+#             serializer_class = shelterSerializer
+#             serializer = serializer_class(data=request.data)
+        
+#         if serializer.is_valid():
+#             account = serializer.save()
+#             user_id = account.id
+#             if user_type == 'seeker': 
+#                 data = {
+#                     "response": "Success",
+#                     "user_id": user_id,
+#                     "username": account.username,
+#                     "email": account.email,
+#                     "first_name": account.first_name,
+#                     "last_name": account.last_name,
+#                     "phone": account.phone,
+#                     "is_shelter": account.is_shelter
+#                 }
+#             else: 
+#                 print(serializer)
+#                 data = {
+#                     "response": "Success",
+#                     "user_id": account.user.id,  
+#                     "username": account.user.username, 
+#                     "shelter_name": account.user.first_name, 
+#                     "charity_id": account.charity_id,
+#                     "address": account.address,
+#                     "email": account.user.email,  
+#                     "phone": account.user.phone,
+#                     "is_shelter": account.user.is_shelter 
+#                 }
+
+#             return Response(data)
+#         else:
+#             return Response(serializer.errors)
+        
+
+# from rest_framework import status
+# from django.contrib.auth import update_session_auth_hash
+
+# class UpdateView(generics.RetrieveUpdateAPIView):
+
+#     def get_serializer_class(self):
+#         user_type = self.kwargs.get('user_type', 'default')
+#         if user_type == 'seeker':
+#             return userSerializer
+#         else:
+#             return shelterSerializer
+
+#     # serializer_class = get_serializer_class
+#     permission_classes = [IsAuthenticated]
+
+
+#     def get_object(self):
+#         user_type = self.kwargs.get('user_type', 'default')
+
+#         is_shelter = self.request.user.is_shelter
+
+#         if user_type == 'seeker' and not is_shelter:
+#             return self.request.user
+#         elif user_type == 'shelter' and is_shelter:
+#             return self.request.user.shelter_profile
+#         else:
+#             raise PermissionDenied("You are not authorized to edit this profile")
+    
+#     def update(self, request, *args, **kwargs):
+#         kwargs['partial'] = True 
+#         return super().update(request, *args, **kwargs)
+    
+
