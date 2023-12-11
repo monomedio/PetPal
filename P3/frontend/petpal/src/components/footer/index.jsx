20a06f80fc42915a2fdd93d64e163f958cb12a23
Anisha Latchman <anishalatchman@gmail.com>
2023-12-11 13:03:40 -0500

    added footer component

---
+++ P3/frontend/petpal/src/components/footer/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "../../assets/images/petpal-logo.svg";
+import "./style.css";
+
+export default function Footer() {
+    return (
+        <footer>
+        <nav class="navbar footer-nav">
+            <div class="container-fluid justify-content-center">
+            <div
+                class="row justify-content-center justify-content-lg-between container-fluid mx-0 px-0"
+            >
+                <div
+                class="container-fluid col-lg-6 px-0 pt-2 pt-lg-0 d-flex align-items-center justify-content-center justify-content-lg-start"
+                >
+                <a href="./index.html" class="navbar-brand"
+                    ><img id="footer-logo" src={logo} alt="logo"/></a>
+                </div>
+                <div
+                class="col-lg-6 d-flex justify-content-center justify-content-lg-end px-0"
+                >
+                <nav class="d-flex align-items-center flex-lg-row flex-column">
+                    <a
+                    class="nav-link footer-nav-item px-lg-5 py-2 py-lg-0"
+                    href="./search-page.html"
+                    >Adopt A Pet</a>
+                    <a
+                    class="nav-link footer-nav-item pb-2 pb-lg-0"
+                    href="./applications.html"
+                    >View Applications</a>
+                </nav>
+                </div>
+            </div>
+            <div class="row mt-lg-2">
+                <div class="col">
+                <p id="copyright">
+                    Copyright &copy; 2023 by Anisha, Anne, Celina, Sam
+                </p>
+                </div>
+            </div>
+            </div>
+        </nav>
+        </footer>
+    )
+
+}
--- P3/frontend/petpal/src/pages/applications/index.jsx
+++ P3/frontend/petpal/src/pages/applications/index.jsx
@@ -1,11 +1,132 @@
 import React from "react";
 import NavBar from "../../components/navbar";
+import Footer from "../../components/footer";
+import duoduo from "../../assets/images/duoduo/duoduo1.jpg";
+import fluffy from "../../assets/images/duoduo/duoduo2.jpg";
+import shelter_pic from "../../assets/images/shelter-images/toronto-humane-society.png";
+import profile_pic from "../../assets/images/among-us.jpeg";
+import "./style.css";
 
 export default function Applications() {
     return (
-        <div>
-            <NavBar />
-            <h1>Applications</h1>
+        <>
+        <body class="light-blue">
+
+        <NavBar />
+        <div class="my-3 container white rounded-item">
+            <div class="row">
+            <div class="col-12 col-md-3 light-orange h-120 p-0">
+                <h3 class="ms-3">All Applications</h3>
+                <a href="./applications.html">
+                <div class="orange d-flex align-items-center w-100">
+                    <img
+                    src={duoduo}
+                    alt="Profile pic"
+                    class="rounded-circle m-2"
+                    width="50px"
+                    height="50px"
+                    />
+                    <p class="text-black m-0">Application for Duo Duo</p>
+                </div>
+                </a>
+                <a href="./applications-fluffy.html">
+                <div
+                    class="light-orange d-flex align-items-center w-100 border-tab"
+                >
+                    <img
+                    src={fluffy}
+                    alt="Profile pic"
+                    class="rounded-circle m-2"
+                    width="50px"
+                    height="50px"
+                    />
+                    <p class="text-black m-0">Application for Fluffy</p>
+                </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-9 h-100 p-0">
+                <div class="d-flex w-100 align-items-center border-bottom">
+                <img
+                    src={duoduo}
+                    alt="Profile pic"
+                    class="rounded-circle m-3"
+                    width="100px"
+                    height="100px"
+                />
+                <div>
+                    <h3 class="mt-0">Application For Duo Duo</h3>
+                    <div class="d-flex">
+                    <p class="teal rounded-item px-3 m-0 me-3">Pending</p>
+                    <p class="text-black m-0">Submitted on 10/10/2023</p>
+                    </div>
+                </div>
+                </div>
+                <div class="d-flex w-100 align-items-center border-bottom grey">
+                <p class="text-black m-3"><b>Name:</b> Anisha Latchman</p>
+                <p class="text-black m-3">
+                    <b>Email:</b> anisha.latchman@utoronto.ca
+                </p>
+                <p class="text-black m-3"><b>Phone:</b> +1 (123) 456-7890</p>
+                <p class="text-black m-3">
+                    <b>Address:</b> 123 Street, City, PROV, POSTAL
+                </p>
+                </div>
+                <div class="d-flex w-100 align-items-center border-bottom grey">
+                <p class="text-black m-3">
+                    <b>Message:</b> Hi, I am interested in adopting Duo Duo. I have
+                    2 years of experience with cats and I live in a high-rise
+                    apartment.
+                </p>
+                </div>
+                <h3 class="ms-4">Chat</h3>
+                <div class="container chat-container">
+                <div>
+                    <div class="d-flex w-100 align-items-center">
+                    <img
+                        src={shelter_pic}
+                        alt="Profile pic"
+                        class="rounded-circle ms-3"
+                        width="40px"
+                        height="40px"
+                    />
+                    <p class="chat-msg ms-2">Hello! Nice to meet you!</p>
+                    </div>
+                    <p class="chat-sent-name left">Humane Society</p>
+                </div>
+                <div>
+                    <div
+                    class="d-flex w-100 align-items-center justify-content-end"
+                    >
+                    <p class="chat-msg ms-2">You as well. Duo Duo is adorable.</p>
+                    <img
+                        src={profile_pic}
+                        alt="Profile pic"
+                        class="rounded-circle ms-3"
+                        width="40px"
+                        height="40px"
+                    />
+                    </div>
+                    <p class="text-end chat-sent-name right">You</p>
+                </div>
+                <div
+                    class="d-flex justify-content-center align-items-center my-3"
+                >
+                    <input
+                    type="text"
+                    id="chat-message"
+                    name="message"
+                    class="form-control login-username p-2 me-2"
+                    placeholder="Type your message here"
+                    required
+                    />
+                    <a class="button-fill" href="/">Send</a>
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
+        </body>
+        <Footer />
+    </>
     )
 }
