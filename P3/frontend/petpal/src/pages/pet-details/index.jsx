11a0f8792eb2cc41a5ce5f0b983acc145c448504
Celina <celinayueh12@gmail.com>
2023-12-12 00:08:44 -0500

    Removed uneeded import

--- P3/frontend/petpal/src/pages/pet-details/index.jsx
+++ P3/frontend/petpal/src/pages/pet-details/index.jsx
@@ -4,7 +4,6 @@ import NavBar from "../../components/navbar";
 import PetCarousel from "../../components/PetCarousel";
 import PetIntro from "../../components/PetIntro";
 import PetInfoBox from "../../components/PetInfoBox";
-import { useMediaQuery } from 'react-responsive';
 
 export const url = 'http://localhost:8000'
 
@@ -12,16 +11,11 @@ export default function PetDetails() {
     const { id } = useParams();
     const authToken = localStorage.getItem('authToken');
 
-    // const isSmallScreen = useMediaQuery({ query: '(max-width: 576px)' });
-    // const isLargeScreen = useMediaQuery({ query: '(min-width: 577px)' });
     
     return (
         <div>
             <NavBar />
             <PetIntro id={id} authToken={authToken}/>
-            {/* {isSmallScreen && <PetCarousel id={id} authToken={authToken} variant="sm" />}
-            {isLargeScreen && <PetCarousel id={id} authToken={authToken} variant="lg" />} */}
-
             <PetCarousel id={id} authToken={authToken} variant="sm" />
             <PetCarousel id={id} authToken={authToken} variant="lg" />
             <PetInfoBox id={id} authToken={authToken}/>
