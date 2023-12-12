dc34940614c4005f3c9c5584530df7bd15a207bc
Celina <celinayueh12@gmail.com>
2023-12-11 20:03:56 -0500

    added pet carousel

--- P3/frontend/petpal/src/components/pet-carousel/index.jsx
+++ P3/frontend/petpal/src/components/pet-carousel/index.jsx
@@ -5,20 +5,39 @@ import {getPet, getPetImage} from '../../api/pet-info.js';
 import "./style.css"
 export const url = 'http://localhost:8000'
 
-function PetCarousel() {
+const PetCarousel = ({targetId, variant}) => {
     const [data, setData] = useState();
-    const [images, setImages] = useState();
-    const { id } = useParams();
+    const [images, setImages] = useState([]);
 
 
     const getData = useCallback(async () => {
-        const data = await getPet(id);
-        const images = await getPetImage(id);
+        const data = await getPet(targetId);
+        const images = await getPetImage(targetId);
         console.log('Data:', data);
         console.log('Images:', images);
         setData(data)
         setImages(images)
-    }, [id])
+    }, [targetId])
+
+    const generateCarouselItems = () => {
+        return images.map((image, index) => (
+          <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+            <div className={`col-12 row${variant === 'lg' ? ' ' : '-4'}`}>
+              {variant === 'lg' ? (
+                images.map((image, i) => (
+                  <div key={i} className="col-4 carousel-img">
+                    <img src={image.image} className="d-block w-100" alt={`Pet Picture ${i + 1}`} />
+                  </div>
+                ))
+              ) : (
+                <div className="col carousel-img">
+                  <img src={image.image} className="d-block w-100" alt={`Pet Picture ${index + 1}`} />
+                </div>
+              )}
+            </div>
+          </div>
+        ));
+      };
 
 
     useEffect(() => {
@@ -39,20 +58,107 @@ function PetCarousel() {
         }, [getData]);
 
         return (
-            images && images.length > 0 && (
-            <div className="carousel-inner">
-                {images.map((image, index) => (
-                <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                    <div className="col-12 row">
-                    <div className="col carousel-img">
-                        <img src={image.image} className="d-block w-100" alt={`Pet Picture ${index + 1}`} />
-                    </div>
-                    </div>
-                </div>
+            <div id={targetId} className={`carousel slide margin-v ${variant === 'lg' ? 'mx-3' : ''}`} data-bs-ride="carousel">
+              <div className="carousel-indicators">
+                {images && images.length > 0 && images.map((image, index) => (
+                    <button
+                    key={index}
+                    type="button"
+                    data-bs-target={`#${targetId}`}
+                    data-bs-slide-to={index}
+                    className={index === 0 ? 'active' : ''}
+                    aria-label={`Slide ${index + 1}`}
+                    ></button>
                 ))}
+                </div>
+              <div className="carousel-inner">
+                {generateCarouselItems()}
+              </div>
+              {variant === 'lg' && (
+                <>
+                  <button className="carousel-control-prev" type="button" data-bs-target={`#${targetId}`} data-bs-slide="prev">
+                    <img src="../assets/images/left-paw.svg" alt="Previous" />
+                    <span className="visually-hidden">Previous</span>
+                  </button>
+                  <button className="carousel-control-next" type="button" data-bs-target={`#${targetId}`} data-bs-slide="next">
+                    <img className="img-fluid" src="../assets/images/right-paw.svg" alt="Next" />
+                    <span className="visually-hidden">Next</span>
+                  </button>
+                </>
+              )}
             </div>
-            )
-        );
+          );
+
+        // return (
+        //     // Small Carousel
+        //     <>
+        //     <div id="pet-images-sm" className="carousel slide margin-v" data-bs-ride="carousel">
+        //         <div className="carousel-inner">
+        //             {images && images.length > 0 && (
+        //             <>
+        //                 {images.map((image, index) => (
+        //                 <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+        //                     <div className="col-12 row">
+        //                     <div className="col carousel-img">
+        //                         <img src={image.image} className="d-block w-100" alt={`Pet Picture ${index + 1}`} />
+        //                     </div>
+        //                     </div>
+        //                 </div>
+        //                 ))}
+        //             </>
+        //             )}
+        //         </div>
+        //     </div>
+
+        //     // Big Carousel
+        //     <div id={targetId} className={`carousel slide margin-v ${variant === 'lg' ? 'mx-3' : ''}`} data-bs-ride="carousel">
+        //         <div className="carousel-indicators">
+        //         {images.map((_, index) => (
+        //             <button
+        //             key={index}
+        //             type="button"
+        //             data-bs-target={`#${targetId}`}
+        //             data-bs-slide-to={index}
+        //             className={index === 0 ? 'active' : ''}
+        //             aria-label={`Slide ${index + 1}`}
+        //             ></button>
+        //         ))}
+        //         </div>
+        //         <div className="carousel-inner">
+        //         {images.map((image, index) => (
+        //             <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+        //             <div className={`col-12 row${variant === 'lg' ? ' ' : '-4'}`}>
+        //                 {variant === 'lg' ? (
+        //                 images.map((image, i) => (
+        //                     <div key={i} className="col-4 carousel-img">
+        //                     <img src={image} className="d-block w-100" alt={`Pet Picture ${i + 1}`} />
+        //                     </div>
+        //                 ))
+        //                 ) : (
+        //                 <div className="col carousel-img">
+        //                     <img src={image} className="d-block w-100" alt={`Pet Picture ${index + 1}`} />
+        //                 </div>
+        //                 )}
+        //             </div>
+        //             </div>
+        //         ))}
+        //         </div>
+        //         {variant === 'lg' && (
+        //         <>
+        //             <button className="carousel-control-prev" type="button" data-bs-target={`#${targetId}`} data-bs-slide="prev">
+        //             <img src="../assets/images/left-paw.svg" alt="Previous" />
+        //             <span className="visually-hidden">Previous</span>
+        //             </button>
+        //             <button className="carousel-control-next" type="button" data-bs-target={`#${targetId}`} data-bs-slide="next">
+        //             <img className="img-fluid" src="../assets/images/right-paw.svg" alt="Next" />
+        //             <span className="visually-hidden">Next</span>
+        //             </button>
+        //         </>
+        //         )}
+        //     </div>
+        //   </>
+            
+        // );
 };
 
 export default PetCarousel;
